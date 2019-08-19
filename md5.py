b8db83f4bc536e54d339b71b5825c340a024c428
joe192168 <joe192168@sina.com>
2019-08-19 19:34:45 +0800

    update

---
+++ md5.py
@@ -0,0 +1,11 @@
+import hashlib
+
+#md5，其实就是一种算法。可以将一个字符串，或文件，或压缩包，执行md5后，就可以生成一个固定长度为128bit的串。这个串，基本上是唯一的。
+
+#加密明文
+str = input("请输入要加密明文：")
+hl = hashlib.md5()# 创建md5对象
+hl.update(str.encode(encoding='utf-8'))# 若写法为hl.update(str)  报错为： Unicode-objects must be encoded before hashing
+
+print('MD5加密前为 ：' + str)
+print('MD5加密后为 ：' + hl.hexdigest())#MD5加密后
